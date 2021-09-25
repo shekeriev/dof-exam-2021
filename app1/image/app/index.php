b31efe65adf478b57c91bd8a5668e9aca9cd9001
Dimitar Zahariev <dimitar@zahariev.pro>
2021-09-25 08:51:33 +0300

    Update index.php

--- app1/image/app/index.php
+++ app1/image/app/index.php
@@ -1,11 +1,23 @@
+<html>
+	<head>
+		<title>Hello to You</title>
+	</head>
+	<body style="background-color: #ffffff;">
+		<div align="center">
+		
 <?php
 
   if (!isset($_ENV["APP_VARIABLE"])) 
   {
-    print "Hello, Brave One!\nIt appears that you forgot to initialize the APP_VARIABLE variable ...\n";
+    print "<h2>Hello, Brave One!</h2>\n";
+    print "Hm, it appears that you forgot to initialize the <b>APP_VARIABLE</b> variable ...\n";
   }
   else
   {
-    print "Hello, ".$_ENV["APP_VARIABLE"]."!\nCongratulations! You made it! :)\n";
+    print "<h2>Hello, ".$_ENV["APP_VARIABLE"]."!</h2>\n";
+    print "<h3>Congratulations! You made it! :)</h3>\n";
   }
 ?>
+		</div>
+	</body>
+</html>
